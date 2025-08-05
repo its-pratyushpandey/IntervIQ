adbca523df79c738a48ab5f30421395cb2e689b5
Pratyush Kumar Pandey <2300030557@kluniversity.in>
2025-08-05 07:32:40 +0530

    Update container.tsx

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -5,12 +5,15 @@ interface ContainerProps {
   className?: string;
 }
 
+
 export const Container = ({ children, className }: ContainerProps) => {
   return (
+    
     <div
       className={cn("container mx-auto px-4 md:px-8 py-4 w-full", className)}
     >
       {children}
     </div>
+    
   );
 };
