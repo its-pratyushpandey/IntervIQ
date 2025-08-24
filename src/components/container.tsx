450bc581aac75e16c30020b854e7c7685744b87f
Pratyush Kumar Pandey <2300030557@kluniversity.in>
2025-08-24 23:15:59 +0530

    Clean up Container component formatting

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -6,8 +6,6 @@ interface ContainerProps {
 }
 export const Container = ({ children, className }: ContainerProps) => {
   return (
-
-    
     <div
       className={cn("container mx-auto px-4 md:px-8 py-4 w-full", className)}
     >
