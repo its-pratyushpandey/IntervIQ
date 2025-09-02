4bd389e5a2dbda79fb72c3ca05851ae8bc781ed2
Pratyush Kumar Pandey <2300030557@kluniversity.in>
2025-09-02 23:08:20 +0530

    Update firebase.config.ts

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,9 +1,6 @@
 import { getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
-
-
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
