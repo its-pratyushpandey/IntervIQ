2aab3ddab0149cf4456df987cf9f29387d7be7a6
Pratyush Kumar Pandey <2300030557@kluniversity.in>
2025-11-04 23:56:59 +0530

    Remove unnecessary whitespace in header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,6 @@ import { NavigationRoutes } from "./navigation-routes";
 import { NavLink } from "react-router-dom";
 import { ProfileContainer } from "./profile-container";
 import { ToggleContainer } from "./toggle-container";
-
 const Header = () => {
   const { userId } = useAuth();
   return (
